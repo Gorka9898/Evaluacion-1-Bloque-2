0ed2d9c415779134293ea94139341db46061d9c7
Gorka9898 <74233040+Gorka9898@users.noreply.github.com>
2021-10-04 12:45:01 +0200

    4 añadido

---
+++ Ej4.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="UTF-8">
+
+<body>
+
+<?php 
+
+
+    class Numeros {
+        private $lineas;
+        private $columnas;
+
+        public function __construct() {
+
+            
+        }
+        
+
+
+        public function calcNumeros() {
+
+          
+
+
+            }
+
+           
+
+    
+
+    }
+    $Numeros = new Numeros(3);
+    $Numeros->calcNumeros();
+
+
+?>
+
+
+</body>
+
+</html>
